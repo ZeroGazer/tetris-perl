db4aeec68742e9bd38e6065370e1fb5903e00ab7
hlchanad <hlchanad@stu.ust.hk>
2015-04-18 01:39:34 +0800

    Update tetris.pl
    can move right or left (with constraints)
    to enable auto dropping, uncomment line 40
    there are 2 red blocks for testing 
    i didnt follow that code to use heap(?)
    instead, i use a stupid 2d array to store things

--- tetris.pl
+++ tetris.pl
@@ -29,21 +29,24 @@ my @patterns = (["***",
 my @colors = qw(#0000FF #00FF00 #FF0000 #FFFF00 #FF00FF #00FFFF);
 
 my @currentBlock;
+my @currentPattern;
+my @currentBlockCoors; # x0, y0, x1, y1; 0 : left up; 1 : right bottom (in terms of the grid)
+my @heights;		   # height of each columns; eg if column1 has 5 tiles with 1 space in between, heights[1] = 6; 
+my @board;
 
 sub update{
 	#my $newBlock = $wGame->createRectangle(0, 0, $TILE_SIZE, $TILE_SIZE, '-fill' => '#00FF00', '-tag' => 'block');
-	print "\$gameover = $gameover\n";
 	if (!$gameover){
-		moveDown();
+		#moveDown();
 		$wBase->after($updateInterval, \&update);
 	}
 }
 
-sub start {
+sub start{
 	$wBase->after($updateInterval, \&update);
 }
 
-sub createScreen {
+sub createScreen{
     $wBase = MainWindow->new(-title => 'Tetris - Perl/Tk');
 
     $wGame = $wBase->Canvas('-width'  => $MAX_COLS * $TILE_SIZE,
@@ -62,12 +65,120 @@ sub createScreen {
     $wQuitBitton->pack('-side'=> 'right', '-fill' => 'y', '-expand' => 'y');
 }
 
-sub moveLeft{
-	print "pressed left arrow\n";
+sub moveRight{
+	print "pressed right arrow : ($currentBlockCoors[2] < $MAX_COLS)?\n";
+	
+	if ($currentBlockCoors[2] < $MAX_COLS-1){
+		
+		for my $i (0..scalar(@currentPattern)-1){
+			my $line = $currentPattern[$i];
+			my @line = split(//, $line);
+			
+			my $xOffset = $currentBlockCoors[0];
+			my $yOffset = $currentBlockCoors[1];
+			for my $j (0..length($line)-1){
+				my $k = length($line)-1-$j;
+				if ($line[$k] eq "*") {
+					if (${$board[$i+$yOffset]}[$xOffset+$k+1]) {return;} # if a cell right is filled, return with doing nth
+					last;
+				}
+			}
+		}
+	
+		# change @board data to 0
+		for my $i (0..scalar(@currentPattern)-1){
+			my $line = $currentPattern[$i];
+			my @line = split(//, $line);
+			
+			my $xOffset = $currentBlockCoors[0];
+			my $yOffset = $currentBlockCoors[1];
+			for my $j (0..length($line)-1){
+				if ($line[$j] eq "*") {
+					${$board[$i+$yOffset]}[$xOffset+$j] = 0;
+				}
+			}
+		}
+	
+		# move the tile
+		foreach my $unit (@currentBlock){
+			$wGame->move($unit, $TILE_SIZE, 0);
+		}
+		$currentBlockCoors[0] += 1;
+		$currentBlockCoors[2] += 1;
+		
+		# change @board data to 1
+		for my $i (0..scalar(@currentPattern)-1){
+			my $line = $currentPattern[$i];
+			my @line = split(//, $line);
+			
+			my $xOffset = $currentBlockCoors[0];
+			my $yOffset = $currentBlockCoors[1];
+			for my $j (0..length($line)-1){
+				if ($line[$j] eq "*") {
+					${$board[$i+$yOffset]}[$xOffset+$j] = 1;
+				}
+			}
+		}
+	}
+	#printBoard();
 }
 
-sub moveRight{
-	print "pressed right arrow\n";
+sub moveLeft{
+	print "pressed left arrow : ($currentBlockCoors[0] > 0)?\n";
+	
+	if ($currentBlockCoors[0] > 0){
+		
+		for my $i (0..scalar(@currentPattern)-1){
+			my $line = $currentPattern[$i];
+			my @line = split(//, $line);
+			
+			my $xOffset = $currentBlockCoors[0];
+			my $yOffset = $currentBlockCoors[1];
+			for my $j (0..length($line)-1){
+				if ($line[$j] eq "*") {
+				print ("\${\$board[", $i+$yOffset, "]}[", $xOffset+$j-1, "] = ${$board[$i+$yOffset]}[$xOffset+$j-1] ");
+					if (${$board[$i+$yOffset]}[$xOffset+$j-1]) {return;} # if a cell left is filled, return with doing nth
+					last;
+				}
+			}
+			print "\n";
+		}
+	
+		# change @board data to 0
+		for my $i (0..scalar(@currentPattern)-1){
+			my $line = $currentPattern[$i];
+			my @line = split(//, $line);
+			
+			my $xOffset = $currentBlockCoors[0];
+			my $yOffset = $currentBlockCoors[1];
+			for my $j (0..length($line)-1){
+				if ($line[$j] eq "*") {
+					${$board[$i+$yOffset]}[$xOffset+$j] = 0;
+				}
+			}
+		}
+	
+		foreach my $unit (@currentBlock){
+			$wGame->move($unit, -$TILE_SIZE, 0);
+		}
+		$currentBlockCoors[0] -= 1;
+		$currentBlockCoors[2] -= 1;
+		
+		# change @board data to 1
+		for my $i (0..scalar(@currentPattern)-1){
+			my $line = $currentPattern[$i];
+			my @line = split(//, $line);
+			
+			my $xOffset = $currentBlockCoors[0];
+			my $yOffset = $currentBlockCoors[1];
+			for my $j (0..length($line)-1){
+				if ($line[$j] eq "*") {
+					${$board[$i+$yOffset]}[$xOffset+$j] = 1;
+				}
+			}
+		}
+	}
+	#printBoard();
 }
 
 sub moveDown{
@@ -77,38 +188,97 @@ sub moveDown{
 	# TODO : check touched ground or not
 	
 	if (!$touchedGround){
+	
+		# change @board data to 0
+		for my $i (0..scalar(@currentPattern)-1){
+			my $line = $currentPattern[$i];
+			my @line = split(//, $line);
+			
+			my $xOffset = $currentBlockCoors[0];
+			my $yOffset = $currentBlockCoors[1];
+			for my $j (0..length($line)-1){
+				if ($line[$j] eq "*") {
+					${$board[$i+$yOffset]}[$xOffset+$j] = 0;
+				}
+			}
+		}
+	
 		foreach my $unit (@currentBlock){
 			$wGame->move($unit, 0, $TILE_SIZE);
 		}
+		$currentBlockCoors[1] += 1;
+		$currentBlockCoors[3] += 1;
+		
+		# change @board data to 1
+		for my $i (0..scalar(@currentPattern)-1){
+			my $line = $currentPattern[$i];
+			my @line = split(//, $line);
+			
+			my $xOffset = $currentBlockCoors[0];
+			my $yOffset = $currentBlockCoors[1];
+			for my $j (0..length($line)-1){
+				if ($line[$j] eq "*") {
+					${$board[$i+$yOffset]}[$xOffset+$j] = 1;
+				}
+			}
+		}
 	}
+	#printBoard();
 }
 
 sub fallDown{
 	print "pressed spacebar \n";
 }
 
-sub drawLines {
+sub drawLines{
 	for my $i (0 .. $MAX_ROWS){
 		$wGame->createLine(0, $i*$TILE_SIZE, $MAX_COLS*$TILE_SIZE, $i*$TILE_SIZE, '-fill' => 'black');}
 	for my $i (0 .. $MAX_COLS){
 		$wGame->createLine($i*$TILE_SIZE, 0, $i*$TILE_SIZE, $MAX_ROWS*$TILE_SIZE, '-fill' => 'black');}
 }
 
+sub printBoard{
+	foreach my $row (@board){
+		foreach my $cell (@$row){
+			print "$cell ";
+		}
+		print "\n";
+	}
+}
+
 sub createTile{
     my $color   = $colors[int(rand (scalar (@colors)))];
     my $pattern = $patterns[int(rand (scalar(@patterns)))];
+	my $xOffset, my $height = scalar(@$pattern), my $width;
+	
 	for my $i (0..scalar(@$pattern)-1){
 		my $line = @$pattern[$i];
 		my @line = split (//, $line);
-		my $xOffset = int(($MAX_COLS - length($line)) / 2);
+		$xOffset = int(($MAX_COLS - length($line)) / 2);
+		$width = scalar(@line);
 		
 		for my $j (0..scalar(@line)-1){
 			my $char = @line[$j];
 			if ($char eq "*"){
+				# set 1 in @board
 				my $unit = $wGame->createRectangle(($j+$xOffset)*$TILE_SIZE, $i*$TILE_SIZE, ($j+$xOffset+1)*$TILE_SIZE, ($i+1)*$TILE_SIZE, '-fill' => $color);
 				push @currentBlock, $unit;
+				${$board[$i]}[$j+$xOffset] = 1;
 			}
 		}
+	}
+	printBoard();
+	@currentPattern = @$pattern;
+	@currentBlockCoors = ($xOffset, 0, $width+$xOffset-1, $height-1);
+}
+
+sub clearBoard{
+	for my $i (0..$MAX_ROWS-1){
+		my @temp;
+		for my $j (0..$MAX_COLS-1){
+			push (@temp, 0);
+		}
+		push (@board, \@temp);
 	}
 }
 
@@ -120,7 +290,14 @@ sub init{
 	$wBase->bind('<KeyPress-Right>', \&moveRight);
 	$wBase->bind('<KeyPress-Down>', \&moveDown);
 	$wBase->bind('<KeyPress-space>', \&fallDown);
+	clearBoard();
+	# the following 4 lines are for testing
+	${$board[1]}[7] = 1;
+	${$board[1]}[1] = 1;
+	$wGame->createRectangle(7*$TILE_SIZE, $TILE_SIZE, 8*$TILE_SIZE, 2*$TILE_SIZE, '-fill'=>'#FF0000');
+	$wGame->createRectangle($TILE_SIZE, $TILE_SIZE, 2*$TILE_SIZE, 2*$TILE_SIZE, '-fill'=>'#FF0000');
 	createTile();
+	
 	
 	
 	start();
